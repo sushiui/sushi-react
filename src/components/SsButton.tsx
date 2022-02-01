34d2a0635279295f579a0449fb39ee7f475a44b0
Apirak Panatkool <apirakb@gmail.com>
2022-02-01 07:26:26 +0700

    create button page

--- src/components/SsButton.tsx
+++ src/components/SsButton.tsx
@@ -1,14 +1,35 @@
 import { MouseEventHandler } from "react";
 
-type Props = {
-  onClick: MouseEventHandler,
-  text: string,
+type ButtonProps = {
+  onClick?: MouseEventHandler,
+  children?: React.ReactNode,
+  label?: string,
+  leftIcon?: string,
+  rightIcon?: string,
+  primary?: boolean,
 }
 
-const Button = ({ onClick, text}: Props) => (
-  <button onClick={onClick}>
-    {text}
-  </button>
-)
+export default function Button({
+    onClick,
+    label,
+    children,
+    leftIcon,
+    rightIcon,
+    primary,
+  }: ButtonProps) {
 
-export default Button;
+  const spanLeftIcon:JSX.Element = leftIcon ? <span className={`ss-icon-${leftIcon}`} /> : <></>;
+  const spanRightIcon:JSX.Element = rightIcon ? <span className={`ss-icon-${rightIcon}`} /> : <></>;
+  const content:string|React.ReactNode = label ? label : children;
+
+  console.log(primary);
+  const className = primary ? "" : "-secondary";
+
+  return (
+    <button className={className} onClick={onClick}>
+      {spanLeftIcon}
+      {content}
+      {spanRightIcon}
+    </button>
+  )
+};
--- src/pages/ButtonPage.tsx
+++ src/pages/ButtonPage.tsx
@@ -1,6 +1,8 @@
 import SsPage from "../components/SsPage";
 import SsHeader from "../components/SsHeader";
 import SsBody from "../components/SsBody";
+import SsCard from "../components/SsCard";
+import SsButton from "../components/SsButton";
 
 const ButtonPage = () => {
   return (
@@ -9,7 +11,20 @@ const ButtonPage = () => {
         <span className='label'>Button</span>
       </SsHeader>
       <SsBody>
-        Button
+        <SsCard title='&lt;Button&gt;'>
+          <SsBody>
+             <SsButton label="Primary Button"></SsButton>
+             <SsButton>Another Label</SsButton>
+             <SsButton leftIcon='Search' rightIcon='Search'>Another Label</SsButton>
+          </SsBody>
+        </SsCard>
+        <SsCard title='&lt;Button&gt;'>
+          <SsBody>
+             <SsButton primary label="Primary Button"></SsButton>
+             <SsButton primary >Another Label</SsButton>
+             <SsButton primary leftIcon='Search' rightIcon='Search'>Another Label</SsButton>
+          </SsBody>
+        </SsCard>
       </SsBody>
     </SsPage>
   );
