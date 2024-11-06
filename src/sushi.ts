d99a0e3759b7a6619754b71be5b4c001760487be
Apirak Panatkool <apirakb@gmail.com>
2024-11-06 10:58:13 +0700

    Sadly public my access key T_T

--- src/sushi.ts
+++ src/sushi.ts
@@ -1,6 +1,24 @@
-export function changeBackground() {
-  console.log("in change background");
-  // var ssContainer = document.getElementsByClassName('ss-container')[0] as HTMLElement;
-  var siteBody = document.querySelector('.ss-site .body') as HTMLElement;
-  siteBody.style.backgroundImage = "url('https://source.unsplash.com/1440x1440?sig=" + Math.random();
+export async function changeBackground() {
+  console.log("Changing background image");
+
+  const siteBody = document.querySelector('.ss-site .body') as HTMLElement;
+  // [TODO] Remove key from unsplash
+  // [TODO] Deploy with github action
+  // [TODO] Move secret to env
+  // const accessKey = 'YOUR_ACCESS_KEY'; // Replace with your Unsplash Access Key
+  const accessKey = 't1mbpxmurcKa9E9rVOf6E-gcoGKPtW5tnCexrOgETeE';
+
+  try {
+    const response = await fetch(`https://api.unsplash.com/photos/random?orientation=landscape&client_id=${accessKey}`);
+    if (!response.ok) {
+      throw new Error(`Error fetching image: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    const imageUrl = data.urls.regular;
+
+    siteBody.style.backgroundImage = `url('${imageUrl}')`;
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
